a50b5020df8e05089bd5f764831977c85cd2e058
Pepe199111 <piotrpustul1@gmail.com>
2021-06-10 17:51:05 +0100

    added auth project

--- src/components/Projects/ProjectsData.js
+++ src/components/Projects/ProjectsData.js
@@ -3,19 +3,34 @@ import imgFoodOrder from "../../static/images/food-order.png";
 import imgPlanner from "../../static/images/planYourMeal.png";
 import imgPortfolio from "../../static/images/portfolio.png";
 import imgQuotes from "../../static/images/quotes.png";
+import imgAuth from "../../static/images/auth.png";
 
 const myProjects = [
    {
       img: imgRestaurant,
       heading: "The Lockdown",
-      description: "The application allows you to book a table in a restaurant, with the additional function of choosing exactly the one that interests us, not counting on luck that the host will choose the best for us.",
+      description: "The application allows user to book a table in a restaurant, with the additional function of choosing exactly the one that interests us, not counting on luck that the host will choose the best for us.",
       githubLink: "https://github.com/pepe199111/restaurant-reservation/tree/main/restaurant",
       liveLink: "https://pepe199111.github.io/restaurant-reservation/#/",
    },
    {
+      img: imgPortfolio,
+      heading: "Portfolio",
+      description: "The page you are currently on. I have posted some information about me along with my projects. I hope you like it.",
+      githubLink: "https://github.com/pepe199111/Piotr-Pustul-Portfolio",
+      liveLink: "https://pepe199111.github.io/portfolio/#/",
+   },
+   {
+      img: imgAuth,
+      heading: "Authentication",
+      description: "The authentication project allows user to create an account, firebase is used to store user data. Once logged in, the user is granted access to the rest of the site, where the user can change its password in the user panel.",
+      githubLink: "https://github.com/pepe199111/authentication-project",
+      liveLink: "https://auth-project-react-fb605.web.app/",
+   },
+   {
       img: imgFoodOrder,
       heading: "Food order",
-      description: "The app allows you to order food. Features adding products to cart, entering shipping information. Menus and placed orders are retrieved from an external database (Firebase).",
+      description: "The app allows user to order food. Features adding products to cart, entering shipping information. Menus and placed orders are retrieved from an external database (Firebase).",
       githubLink: "https://github.com/pepe199111/food-order",
       liveLink: "https://food-order-70879.firebaseapp.com/",
    },
@@ -32,13 +47,6 @@ const myProjects = [
       description: "This is an application that allows us to plan meals for a given day and week. This is my first project that I worked on together with others programmers, during the course at Coders Lab school, the project was conducted using the Scrum method and JSON server.",
       githubLink: "https://github.com/pepe199111/Nutrition",
       liveLink: "https://pepe199111.github.io/Nutrition/#/",
-   },
-   {
-      img: imgPortfolio,
-      heading: "Portfolio",
-      description: "The page you are currently on. I have posted some information about me along with my projects. I hope you like it.",
-      githubLink: "https://github.com/pepe199111/Piotr-Pustul-Portfolio",
-      liveLink: "https://pepe199111.github.io/portfolio/#/",
    },
 ];
 
