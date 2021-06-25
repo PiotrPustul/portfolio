7df77b21367cd15fa61e34bf7e0d5ad60eca4aef
Pepe199111 <piotrpustul1@gmail.com>
2021-06-25 13:07:42 +0100

    changes in about

--- src/components/About/About-me/AboutMe.js
+++ src/components/About/About-me/AboutMe.js
@@ -3,7 +3,6 @@ const AboutMe = () => {
       <div className="about__section">
          <h3 className="about__heading">Who I am?</h3>
          <p className="about__paragraph">
-            My name is Piotr Pustul. I'm 29 years old and I was born in the south of Poland in Czestochowa.<br />
             I graduated from the Polish school of programming where I learned the necessary technologies to build modern and user-friendly websites such as HTML, CSS and JavaScript.I'm steadily expanding my knowledge by mastering the latest libraries such as Sass, React.JS, Redux, TypeScript. Since then, I've learned a lot and coding has become my passion.I've been constantly exploring the world of web development. I consolidated my knowledge by implementing my own projects. My greatest pleasure is creating applications in React.JS. I usually style these applications using Sass and while keeping in mind the basic principle of mobile-first.
          </p>
          <p className="about__paragraph">
